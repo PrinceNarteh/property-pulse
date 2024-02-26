eb191919d4879725040ef0677cf11cb5e1589978
Prince Narteh <prinarteh@gmail.com>
2024-02-26 03:17:17 +0000

    feat: add Navbar and images

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 import "@/assets/styles/globals.css";
+import { Metadata } from "next";
+import { Navbar } from "@/components";
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Property Pulse | Find The Perfect Rental",
   description: "Find Your Dream Rental Property",
-  keyword: "rental, find rentals, find properties",
+  keywords: "rental, find rentals, find properties",
 };
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
       <body>
+        <Navbar />
         <div>{children}</div>
       </body>
     </html>
