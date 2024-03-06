2c46953c3f92f84b509b2f8bd403e91f164c61cd
Prince Narteh <prinarteh@gmail.com>
2024-03-05 17:21:53 -0800

    feat: add Hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import { Hero } from "@/components";
 import React from "react";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <>
+      <Hero />
+    </>
+  );
 };
 
 export default HomePage;
