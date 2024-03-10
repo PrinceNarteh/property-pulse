13cf0a19888edcdf51e3b77fa93fa8b286f33455
Prince Narteh <prinarteh@gmail.com>
2024-03-09 21:21:39 -0800

    feat: implements PropertyCard

--- types.ts
+++ types.ts
@@ -15,8 +15,9 @@ export type Property = {
   square_feet: number;
   amenities: string[];
   rates: {
-    weekly: number;
-    monthly: number;
+    weekly?: number;
+    monthly?: number;
+    nightly?: number;
   };
   seller_info: {
     name: string;
